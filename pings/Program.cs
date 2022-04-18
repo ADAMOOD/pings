595d3dd587244a8c025e399973231525170810a7
ADAMOOD <99761968+ADAMOOD@users.noreply.github.com>
2022-04-18 21:53:41 +0200

    pinging working

--- pings/Program.cs
+++ pings/Program.cs
@@ -1,36 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Text;
-
+using System.Threading;
 
 namespace pings
 {
     internal class Program
     {
-       const string IPAddress="8.8.8.8";
-       private const int CursorUnderHeadY = 4;
+       const string IPAddress= "8.8.8.8";
+       private const int CursorUnderHeadY = 3;
+       private const int BorderOfPrintingList = 25;
+       private const int DeletedIndex = 0;
+       private const int time = 200;
+       static List<string> Pings = new List<string>();
         static void Main(string[] args)
         {
-           bool onlyStartDate = true;
+            Console.CursorVisible = false;
+            bool onlyStartDate = true;
            bool Cursor = true;
-
-           for (int i = 0; i < 26; i++)
+           while (true)
            {
-               Head(onlyStartDate);
-               onlyStartDate = false;
-               if (Cursor)
+               for (int i = 0; i < BorderOfPrintingList; i++)
                {
-                   setingCursor();
-                   Cursor = false;
-                }
-               Pinging();
+                   Head(onlyStartDate);
+                   onlyStartDate = false;
+                   if (Cursor)
+                   {
+                       setingCursor();
+                       Cursor = false;
+                   }
+                   Pinging();
+               }
+               sleeping();
+               printigList();
             }
         }
         private static void Head(bool checkForDate)
         {
-           
             Console.ForegroundColor = ConsoleColor.Black;
             Console.BackgroundColor = ConsoleColor.White;
             if (checkForDate)
@@ -48,18 +57,37 @@ namespace pings
             var timeOfPing = stopwatch.ElapsedMilliseconds;
             CheckIfPingWasSuccessful(reply, timeOfPing);
         }
-
         private static void CheckIfPingWasSuccessful(PingReply reply, long timeOfPing)
         {
             if (reply.Status.ToString().Equals("Success"))
             {
-               
-                Console.WriteLine($"reply from  {reply.Address}: bytes={reply.Buffer.Length} time={timeOfPing}x{reply.RoundtripTime} TTL={reply.Options.Ttl}");
-
+                AddingInfoAboutPingToList(reply, timeOfPing);
             }
             else
             {
                 Console.WriteLine(reply.Status.ToString());
+            }
+        }
+        private static void AddingInfoAboutPingToList(PingReply reply, long timeOfPing)
+        {
+            var index = Pings.Count;
+            if (index > BorderOfPrintingList)
+            {
+                MoveList(DeletedIndex);
+                setingCursor();
+            }
+            Pings.Add($"reply from  {reply.Address}: bytes={reply.Buffer.Length} time={reply.RoundtripTime}ms TTL={reply.Options.Ttl}");
+        }
+
+        private static void MoveList(int oldIndex)
+        {
+            Pings.RemoveAt(oldIndex);
+        }
+        private static void printigList()
+        {
+            foreach (var VARIABLE in Pings)
+            {
+                Console.WriteLine(VARIABLE);
             }
         }
         private  static void setingCursor()
@@ -67,6 +95,9 @@ namespace pings
             Console.SetCursorPosition(0, CursorUnderHeadY);
         }
 
-     
+        private static void sleeping()
+        {
+            Thread.Sleep(time);
+        }
     }
 }
